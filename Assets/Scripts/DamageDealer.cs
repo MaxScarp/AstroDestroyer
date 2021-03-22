277183110d07929d3e4db94d4673f0bd0a80b000
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-03-22 15:30:41 +0100

    Create altre Waves ed altri Paths ed inseriti nel gameplay. Fatto un po' di tuning del gameplay.

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    [SerializeField] int damage = 50;
+    [SerializeField] int damage = 30;
 
     public int GetDamage() => damage;
 
